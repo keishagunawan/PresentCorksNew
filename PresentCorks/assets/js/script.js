9b271f49ad5b5cd35933db38ff4936b8250fa5ae
bleuEve <cathlauren09@gmail.com>
2023-12-11 10:16:04 +0700

    gajelas nih

--- PresentCorks/assets/js/script.js
+++ PresentCorks/assets/js/script.js
@@ -75,19 +75,19 @@ setInterval(changePosition, 2000);
 
 /*ANIMATION-3*/
 document.addEventListener('DOMContentLoaded', function () {
-    var homepageDesc = document.querySelector('.homepage-desc');
+    let homepageDesc = document.querySelector('.homepage-desc');
 
     // Init ScrollMagic
-    var controller = new ScrollMagic.Controller();
+    let controller = new ScrollMagic.Controller();
 
     // Create GSAP timeline for sub-heading animations
-    var subHeadingTimeline = gsap.timeline();
+    let subHeadingTimeline = gsap.timeline();
     subHeadingTimeline.fromTo('.sub-heading-1', { marginTop: 0 }, { marginTop: 50, opacity: 1 });
     subHeadingTimeline.fromTo('.sub-heading-2', { marginTop: 0 }, { marginTop: 30, opacity: 1 });
     subHeadingTimeline.fromTo('.sub-heading-3', { marginTop: 0 }, { marginTop: 60, opacity: 1 });
 
     // Scene for revealing sub-headings
-    var subHeadingScene = new ScrollMagic.Scene({
+    let subHeadingScene = new ScrollMagic.Scene({
         triggerElement: '.homepage-desc',
         triggerHook: 0,
         duration: '100%' // Adjust duration as needed
@@ -97,16 +97,16 @@ document.addEventListener('DOMContentLoaded', function () {
 
     // Function to create scenes for each sub-heading and desc pair
     function createScene(subHeadingClass, descClass) {
-        var subHeading = document.querySelector(subHeadingClass);
-        var desc = document.querySelector(descClass);
+        let subHeading = document.querySelector(subHeadingClass);
+        let desc = document.querySelector(descClass);
 
         // Scene for sub-heading and desc
-        var scene = new ScrollMagic.Scene({
+        let scene = new ScrollMagic.Scene({
             triggerElement: subHeading,
             triggerHook: 0.7, // Set triggerHook based on your preference
             duration: '100%', // Set the duration as needed
         })
-            .setTween(gsap.fromTo(desc, { opacity: 0, y: 20 }, { opacity: 1, y: 0, duration: 0.5 }, { opacity: 1, y: 0, duration: 0.6 }))
+            .setTween(gsap.fromTo(desc, { opacity: 0, y: 20 }, { opacity: 1, y: 0, duration: 0.5 },{ opacity: 1, y: 0, duration: 0.6 }))
             .addTo(controller);
     }
 
@@ -117,7 +117,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
 
     // Scene to reveal homepage-desc and pin it
-    var pinScene = new ScrollMagic.Scene({
+    let pinScene = new ScrollMagic.Scene({
         triggerElement: '.homepage-desc',
         triggerHook: 0,
         duration: '100%' // Set the duration as needed
@@ -314,168 +314,3 @@ VANTA.FOG({
     scaleMobile: 4
 });
 
-/* SCALE N SIP BANNER & INTERACTIVE */
-
-const banner = document.querySelector('.scaleNsip-banner');
-const bannerimage = banner.querySelector('.bannerimage');
-const bannerVideo = banner.querySelector('.bannervideo');
-
-banner.addEventListener('mouseover', () => {
-    bannerimage.style.opacity = '0'; // Hide the image
-    bannerVideo.play(); // Start playing the video
-});
-
-banner.addEventListener('mouseout', () => {
-    bannerimage.style.opacity = '1'; // Show the image on mouseout
-    bannerVideo.pause(); // Pause the video
-    bannerVideo.currentTime = 0; // Reset video to the beginning
-});
-
-// JavaScript function to update subcategory dropdown based on the selected category
-function updateSubcategories(setNumber) {
-    var categoryDropdown = document.getElementById("categoryDropdown" + setNumber);
-    var subcategoryDropdown = document.getElementById("subcategoryDropdown" + setNumber);
-
-    // Get the selected category and subcategory
-    var selectedCategory = categoryDropdown.options[categoryDropdown.selectedIndex].value;
-
-    // Clear existing options in subcategory dropdown
-    subcategoryDropdown.innerHTML = '';
-
-    // Populate subcategory dropdown based on the selected category
-    switch (selectedCategory) {
-        case "category1":
-            addOption(subcategoryDropdown, "subcat1-1", "16 oz");
-
-            break;
-        case "category2":
-            addOption(subcategoryDropdown, "subcat2-1", "16 oz");
-            addOption(subcategoryDropdown, "subcat2-2", "20 oz");
-            addOption(subcategoryDropdown, "subcat2-3", "40 oz");
-            break;
-        case "category3":
-            addOption(subcategoryDropdown, "subcat3-1", "16 oz");
-            addOption(subcategoryDropdown, "subcat3-2", "20 oz");
-            break;
-        case "category4":
-            addOption(subcategoryDropdown, "subcat4-1", "16 oz");
-            break;
-        case "category5":
-            addOption(subcategoryDropdown, "subcat5-1", "20 oz");
-            break;
-        case "category6":
-            addOption(subcategoryDropdown, "subcat6-1", "16 oz");
-            addOption(subcategoryDropdown, "subcat6-2", "20 oz");
-            addOption(subcategoryDropdown, "subcat6-3", "25 oz");
-            break;
-        case "category7":
-            addOption(subcategoryDropdown, "subcat7-1", "20 oz");
-            break;
-        case "category8":
-            addOption(subcategoryDropdown, "subcat8-1", "20 oz");
-            addOption(subcategoryDropdown, "subcat8-2", "32 oz");
-            break;
-        case "category9":
-            addOption(subcategoryDropdown, "subcat9-1", "16 oz");
-            addOption(subcategoryDropdown, "subcat9-2", "20 oz");
-            addOption(subcategoryDropdown, "subcat9-3", "24 oz");
-            addOption(subcategoryDropdown, "subcat9-4", "25 oz");
-            break;
-        case "category10":
-            addOption(subcategoryDropdown, "subcat10-1", "16 oz");
-            addOption(subcategoryDropdown, "subcat10-2", "20 oz");
-            break;
-
-        // Add cases for other categories and their respective subcategories
-    }
-    updateResultImage(setNumber);
-}
-
-// Helper function to add options to a dropdown
-function addOption(dropdown, value, text) {
-    var option = document.createElement("option");
-    option.value = value;
-    option.text = text;
-    dropdown.add(option);
-}
-
-// Additional functions for updating the result image
-function getImagePath(subcategory) {
-
-    // Customize this function based on your image naming convention and folder structure
-    return "PresentCorks/assets/image/" + subcategory + ".png";
-}
-
-function updateResultImage(setNumber) {
-    var subcategoryDropdown = document.getElementById("subcategoryDropdown" + setNumber);
-    var resultImage = document.getElementById("resultImage" + setNumber);
-
-    var selectedSubcategory = subcategoryDropdown.options[subcategoryDropdown.selectedIndex].value;
-
-    var imagePath = 'assets/image/subcat1-1.png';
-
-
-    if (selectedSubcategory === "subcat1-1") {
-        imagePath = 'assets/image/subcat1-1.png';
-    }
-    else if (selectedSubcategory === "subcat2-1") {
-        imagePath = 'assets/image/subcat2-1.png';
-    }
-    else if (selectedSubcategory === "subcat2-2") {
-        imagePath = 'assets/image/subcat2-2.png';
-    }
-    else if (selectedSubcategory === "subcat2-3") {
-        imagePath = 'assets/image/subcat2-3.png';
-    }
-    else if (selectedSubcategory === "subcat3-1") {
-        imagePath = 'assets/image/subcat3-1.png';
-    }
-    else if (selectedSubcategory === "subcat3-2") {
-        imagePath = 'assets/image/subcat3-2.png';
-    }
-    else if (selectedSubcategory === "subcat4-1") {
-        imagePath = 'assets/image/subcat4-1.png';
-    }
-    else if (selectedSubcategory === "subcat5-1") {
-        imagePath = 'assets/image/subcat5-1.png';
-    }
-    else if (selectedSubcategory === "subcat6-1") {
-        imagePath = 'assets/image/subcat6-1.png';
-    }
-    else if (selectedSubcategory === "subcat6-2") {
-        imagePath = 'assets/image/subcat6-2.png';
-    }
-    else if (selectedSubcategory === "subcat6-3") {
-        imagePath = 'assets/image/subcat6-3.png';
-    }
-    else if (selectedSubcategory === "subcat7-1") {
-        imagePath = 'assets/image/subcat7-1.png';
-    }
-    else if (selectedSubcategory === "subcat8-1") {
-        imagePath = 'assets/image/subcat8-1.png';
-    }
-    else if (selectedSubcategory === "subcat8-2") {
-        imagePath = 'assets/image/subcat8-2.png';
-    }
-    else if (selectedSubcategory === "subcat9-1") {
-        imagePath = 'assets/image/subcat9-1.png';
-    }
-    else if (selectedSubcategory === "subcat9-2") {
-        imagePath = 'assets/image/subcat9-2.png';
-    }
-    else if (selectedSubcategory === "subcat9-3") {
-        imagePath = 'assets/image/subcat9-3.png';
-    }
-    else if (selectedSubcategory === "subcat9-4") {
-        imagePath = 'assets/image/subcat9-4.png';
-    }
-    else if (selectedSubcategory === "subcat10-1") {
-        imagePath = 'assets/image/subcat10-1.png';
-    }
-    else if (selectedSubcategory === "subcat10-2") {
-        imagePath = 'assets/image/subcat10-2.png';
-    }
-
-    resultImage.innerHTML = '<img src="' + imagePath + '" alt= "Selected Image" style="max-width: 100%; height: auto;">';
-
-}
